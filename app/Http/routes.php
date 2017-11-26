ab3ca38dfcfc4a40fc57b395a9e9eb0c3b69954d
yoshiotachibana <ytachibana@mx5.canvas.ne.jp>
2017-11-27 07:53:03 +0900

    user registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,3 +12,6 @@
 */
 
 Route::get('/', 'WelcomeController@index');
+//ユーザ登録
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
--- resources/views/commons/error_messages.blade.php
+++ resources/views/commons/error_messages.blade.php
@@ -1,4 +1,4 @@
-@if (count($erroes) > 0)
+@if (count($errors) > 0)
     @foreach ($errors->all() as $error)
         <div class="alert alert-warning">{{ $error }}</div>
     @endforeach
