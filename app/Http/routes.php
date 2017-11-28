8486ba86df6e1864302f4366f7a5c326eafe1bba
yoshiotachibana <ytachibana@mx5.canvas.ne.jp>
2017-11-29 07:22:41 +0900

    user login

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,3 +15,7 @@ Route::get('/', 'WelcomeController@index');
 //ユーザ登録
 Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
 Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
+
+Route::get('login', 'Auth\AuthController@getLogin')->name('login.get');
+Route::post('login', 'Auth\AuthController@postLogin')->name('login.post');
+Route::get('login', 'Auth\AuthController@getLogout')->name('logout.get');
--- config/app.php
+++ config/app.php
@@ -146,6 +146,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Collective\Html\HtmlServiceProvider::class,
+        Thomaswelton\LaravelGravatar\LaravelGravatarServiceProvider::class,
     ],
 
     /*
@@ -195,6 +196,7 @@ return [
         'View'      => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Gravatar' => Thomaswelton\LaravelGravatar\Facades\Gravatar::class,
 
     ],
 
